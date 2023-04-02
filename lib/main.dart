ec78a9065cd6c00208394d2c56a73cd8982cfc65
Shashwat <100586951+stp2003@users.noreply.github.com>
2023-04-02 18:25:02 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart'
+import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:portfolio/constants.dart';
 import 'package:portfolio/screens/main/home_screen.dart';
